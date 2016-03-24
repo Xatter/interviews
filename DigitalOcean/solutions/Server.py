3838751d251efacc8637967fefe13e582c85fc21
Jim Wallace <xatter@Serenity.local>
2016-03-24 17:28:25 -0400

    first get the business logic correct

---
+++ DigitalOcean/solutions/Server.py
@@ -0,0 +1,71 @@
+import socket
+from typing import List
+
+OK = "OK\n"
+FAIL = "FAIL\n"
+ERROR = "ERROR\n"
+
+class Server(object):
+    def __init__(self):
+        self.INDEX = {}
+        self.INVERSE_INDEX = {}
+
+    def decode(self, message:str):
+        if message[-1] != '\n':
+           return None
+
+        message = message[:-1]
+            
+        parts = message.split('|')
+        dependencies = parts[2].split(',')
+        if dependencies == ['']:
+            dependencies = None
+
+        return (parts[0], parts[1], dependencies)
+
+    def index(self, package:str, dependencies:List[str]):
+        if package in self.INDEX:
+            return OK
+
+        if dependencies is not None:
+            for dependency in dependencies:
+                if dependency not in self.INDEX:
+                    return FAIL
+
+                if dependency not in self.INVERSE_INDEX:
+                    self.INVERSE_INDEX[dependency] = []
+
+                self.INVERSE_INDEX[dependency].append(package)
+
+        self.INDEX[package] = dependencies
+        return OK
+
+    def remove(self, package:str):
+        if package not in self.INDEX:
+            return OK
+
+        if package in self.INVERSE_INDEX:
+            return FAIL
+            
+        self.INDEX.pop(package, None)
+        return OK
+
+    def query(self, package:str):
+        if package in self.INDEX:
+            return OK
+        else:
+            return FAIL
+
+    def begin(self):
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversocket.bind(('localhost', 8080))
+        serversocket.listen(5)
+
+        while True:
+            (clientsocket, address) = serversocket.accept()
+            ct = client_thread(clientsocket)
+            ct.run()
+
+if __name__ == '__main__':
+    s = Server()
+    s.begin()
---
+++ DigitalOcean/solutions/Tests.py
@@ -0,0 +1,87 @@
+import unittest
+import Server
+
+class Test_CommandParsing(unittest.TestCase):
+    # Proper Parse of all command types
+    # Unexepected Input
+
+    def setUp(self):
+        self.server = Server.Server()
+
+    def testSunnyDay(self):
+        command, package, dependnecies = self.server.decode("INDEX|foo|bar\n")
+        self.assertEqual(command, 'INDEX')
+        self.assertEqual(package, 'foo')
+        self.assertCountEqual(dependnecies, ['bar'])
+
+    def test_DependenciesOptional(self):
+        command, package, dependnecies = self.server.decode("INDEX|foo|\n")
+        self.assertEqual(dependnecies, None)
+
+class Test_Indexing(unittest.TestCase):
+# * For `INDEX` commands, the server returns `OK\n` if the package could be indexed or if it was already present. It returns `FAIL\n` if the package cannot be indexed because some of its dependencies aren't indexed yet and need to be installed first.
+    def setUp(self):
+        self.server = Server.Server()
+
+    def test_ShouldIndexSinglePackage(self):
+        self.assertEqual('OK\n', self.server.index('foo', None))
+
+    def test_ShouldReturnOKIfAlreadyExists(self):
+        self.server.index('foo', None)
+        self.assertEqual('OK\n', self.server.index('foo', None))
+
+    def test_ShouldIndexTwoPackages(self):
+        self.assertEqual('OK\n', self.server.index('foo', None))
+        self.assertEqual('OK\n', self.server.index('bar', None))
+
+    def test_ShouldNotIndexIfDependencyNotIndexed(self):
+        self.assertEqual('FAIL\n', self.server.index('bar', ['foo']))
+
+    def test_ShouldIndexWithDependency(self):
+        self.server.index('foo', None)
+        self.assertEqual('OK\n', self.server.index('bar', ['foo']))
+
+class Test_Querying(unittest.TestCase):
+    def setUp(self):
+        self.server = Server.Server()
+
+    def test_QueryFails(self):
+        self.assertEqual('FAIL\n', self.server.query('foo'))
+
+    def test_QueryReturnsOk(self):
+        self.server.index('foo',None)
+        self.assertEqual('OK\n', self.server.query('foo'))
+
+
+# * For `QUERY` commands, the server returns `OK\n` if the package is indexed. It returns `FAIL\n` if the package isn't indexed.
+class Test_Removing(unittest.TestCase):
+# The response code returned should be as follows:
+# * For `REMOVE` commands, the server returns `OK\n` if the package could be removed from the index. 
+#   It returns `FAIL\n` if the package could not be removed from the index because some other indexed package depends on it. 
+#   It returns `OK\n` if the package wasn't indexed.
+
+    def setUp(self):
+        self.server = Server.Server()
+
+    def test_RemoveShouldReturnOkForNonIndexedPackage(self):
+        self.assertEqual('OK\n', self.server.remove('foo'))
+
+    def test_ShouldRemoveSoloPackage(self):
+        self.server.index('foo', None)
+        self.assertEqual('OK\n', self.server.remove('foo'))
+
+    def test_ShouldNotRemoveIfDependencyOfAnotherPackage(self):
+        self.server.index('foo', None)
+        self.server.index('bar', ['foo'])
+        self.assertEqual('FAIL\n', self.server.remove('foo'))
+
+    def test_ShouldNotRemoveIfDependencyOfDependencyOfAnotherPackage(self):
+        # Note to self, we may not need this test case I need some more clarity on the requirements
+        self.server.index('foo', None)
+        self.server.index('bar', ['foo'])
+        self.server.index('baz', ['bar'])
+        self.assertEqual('FAIL\n', self.server.remove('bar'))
+
+class Test_Server(unittest.TestCase):
+    # * If the server doesn't recognise the command or if there's any problem with the message sent by the client it should return `ERROR\n`.
+    pass
