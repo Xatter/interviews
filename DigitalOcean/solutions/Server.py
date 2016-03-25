cc0407f969dc1ffae28d1d6e1f82d3f13cbac2bd
Jim Wallace <xatter@Serenity.local>
2016-03-24 22:49:11 -0400

    all tests pass

--- DigitalOcean/solutions/Server.py
+++ DigitalOcean/solutions/Server.py
@@ -1,22 +1,26 @@
 import socket
+import select
 from typing import List
+import queue
 
 OK = "OK\n"
 FAIL = "FAIL\n"
 ERROR = "ERROR\n"
 
-class Server(object):
+class Indexer(object):
     def __init__(self):
         self.INDEX = {}
         self.INVERSE_INDEX = {}
 
     def decode(self, message:str):
-        if message[-1] != '\n':
-           return None
+        if message.count('|') < 2:
+            return ('ERROR', 'Must be two pipes', None)
 
-        message = message[:-1]
-            
         parts = message.split('|')
+
+        if ',' in parts[1]:
+            return ('ERROR', 'Can not have commas in package field', None)
+
         dependencies = parts[2].split(',')
         if dependencies == ['']:
             dependencies = None
@@ -65,16 +69,78 @@ class Server(object):
         else:
             return FAIL
 
-    def begin(self):
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serversocket.bind(('localhost', 8080))
-        serversocket.listen(5)
-
-        while True:
-            (clientsocket, address) = serversocket.accept()
-            ct = client_thread(clientsocket)
-            ct.run()
+indexer = Indexer()
 
 if __name__ == '__main__':
-    s = Server()
-    s.begin()
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setblocking(0)
+    server.bind(('localhost', 8080))
+    server.listen(10)
+
+    inputs = [ server ]
+    outputs = [ ]
+    message_queues = {}
+
+
+    while inputs:
+        ready_to_read, ready_to_write, exceptional = select.select(inputs, outputs, [])
+
+        for s in ready_to_read:
+            if s is server:
+                connection, client_address = s.accept()
+                connection.setblocking(0)
+                inputs.append(connection)
+
+                message_queues[connection] = queue.Queue()
+            else:
+                data = s.recv(1024)
+                print(s)
+                print(data)
+                if data:
+                    message = data.decode('utf-8').strip()
+                    result = ERROR
+
+                    command, package, dependencies = indexer.decode(message)
+
+                    print(command)
+                    print(package)
+                    print(dependencies)
+
+                    if command == "INDEX":
+                        result = indexer.index(package, dependencies)
+                    elif command == "REMOVE":
+                        result = indexer.remove(package)
+                    elif command == "QUERY":
+                        result = indexer.query(package)
+
+                    print(result)
+                    message_queues[s].put(result)
+                    if s not in outputs:
+                        outputs.append(s)
+                else:
+                    if s in outputs:
+                        outputs.remove(s)
+
+                    inputs.remove(s)
+                    s.close()
+                    del message_queues[s]
+
+        for s in ready_to_write:
+            try:
+                next_msg = message_queues[s].get_nowait()
+            except queue.Empty:
+                outputs.remove(s)
+            except KeyError:
+                pass
+            else:
+                s.send(next_msg.encode('utf-8'))
+
+        for s in exceptional:
+            inputs.remove(s)
+            if s in outputs:
+                outputs.remove(s)
+            s.close()
+
+            del message_queues[s]
+
+
--- DigitalOcean/solutions/Tests.py
+++ DigitalOcean/solutions/Tests.py
@@ -10,51 +10,60 @@ class Test_CommandParsing(unittest.TestCase):
     # Unexepected Input
 
     def setUp(self):
-        self.server = Server.Server()
+        self.indexer = Server.Indexer()
 
     def testSunnyDay(self):
-        command, package, dependnecies = self.server.decode("INDEX|foo|bar\n")
+        command, package, dependnecies = self.indexer.decode("INDEX|foo|bar")
         self.assertEqual(command, 'INDEX')
         self.assertEqual(package, 'foo')
         self.assertCountEqual(dependnecies, ['bar'])
 
     def test_DependenciesOptional(self):
-        command, package, dependnecies = self.server.decode("INDEX|foo|\n")
+        command, package, dependnecies = self.indexer.decode("INDEX|foo|")
         self.assertEqual(dependnecies, None)
+
+    def test_MustHaveTwoPipes(self):
+        command, package, dependnecies = self.indexer.decode("INDEX|foo")
+        self.assertEqual('ERROR', command)
+
+    def test_PackageCantBeList(self):
+        command, package, dependnecies = self.indexer.decode("INDEX|foo,bar,baz|")
+        self.assertEqual('ERROR', command)
+
 
 class Test_Indexing(unittest.TestCase):
 # * For `INDEX` commands, the server returns `OK\n` if the package could be indexed or if it was already present. It returns `FAIL\n` if the package cannot be indexed because some of its dependencies aren't indexed yet and need to be installed first.
     def setUp(self):
-        self.server = Server.Server()
+        self.indexer = Server.Indexer()
 
     def test_ShouldIndexSinglePackage(self):
-        self.assertEqual(OK, self.server.index('foo', None))
+        self.assertEqual(OK, self.indexer.index('foo', None))
 
     def test_ShouldReturnOKIfAlreadyExists(self):
-        self.server.index('foo', None)
-        self.assertEqual(OK, self.server.index('foo', None))
+        self.indexer.index('foo', None)
+        self.assertEqual(OK, self.indexer.index('foo', None))
 
     def test_ShouldIndexTwoPackages(self):
-        self.assertEqual(OK, self.server.index('foo', None))
-        self.assertEqual(OK, self.server.index('bar', None))
+        self.assertEqual(OK, self.indexer.index('foo', None))
+        self.assertEqual(OK, self.indexer.index('bar', None))
 
     def test_ShouldNotIndexIfDependencyNotIndexed(self):
-        self.assertEqual(FAIL, self.server.index('bar', ['foo']))
+        self.assertEqual(FAIL, self.indexer.index('bar', ['foo']))
 
     def test_ShouldIndexWithDependency(self):
-        self.server.index('foo', None)
-        self.assertEqual(OK, self.server.index('bar', ['foo']))
+        self.indexer.index('foo', None)
+        self.assertEqual(OK, self.indexer.index('bar', ['foo']))
 
 class Test_Querying(unittest.TestCase):
     def setUp(self):
-        self.server = Server.Server()
+        self.indexer = Server.Indexer()
 
     def test_QueryFails(self):
-        self.assertEqual(FAIL, self.server.query('foo'))
+        self.assertEqual(FAIL, self.indexer.query('foo'))
 
     def test_QueryReturnsOk(self):
-        self.server.index('foo',None)
-        self.assertEqual(OK, self.server.query('foo'))
+        self.indexer.index('foo',None)
+        self.assertEqual(OK, self.indexer.query('foo'))
 
 
 # * For `QUERY` commands, the server returns `OK\n` if the package is indexed. It returns `FAIL\n` if the package isn't indexed.
@@ -65,35 +74,35 @@ class Test_Removing(unittest.TestCase):
 #   It returns `OK\n` if the package wasn't indexed.
 
     def setUp(self):
-        self.server = Server.Server()
+        self.indexer = Server.Indexer()
 
     def test_RemoveShouldReturnOkForNonIndexedPackage(self):
-        self.assertEqual(OK, self.server.remove('foo'))
+        self.assertEqual(OK, self.indexer.remove('foo'))
 
     def test_ShouldRemoveSoloPackage(self):
-        self.server.index('foo', None)
-        self.assertEqual(OK, self.server.remove('foo'))
+        self.indexer.index('foo', None)
+        self.assertEqual(OK, self.indexer.remove('foo'))
 
     def test_ShouldNotRemoveIfDependencyOfAnotherPackage(self):
-        self.server.index('foo', None)
-        self.server.index('bar', ['foo'])
-        self.assertEqual(FAIL, self.server.remove('foo'))
+        self.indexer.index('foo', None)
+        self.indexer.index('bar', ['foo'])
+        self.assertEqual(FAIL, self.indexer.remove('foo'))
 
     def test_ShouldNotRemoveIfDependencyOfDependencyOfAnotherPackage(self):
         # Note to self, we may not need this test case I need some more clarity on the requirements
-        self.server.index('foo', None)
-        self.server.index('bar', ['foo'])
-        self.server.index('baz', ['bar'])
-        self.assertEqual(FAIL, self.server.remove('bar'))
+        self.indexer.index('foo', None)
+        self.indexer.index('bar', ['foo'])
+        self.indexer.index('baz', ['bar'])
+        self.assertEqual(FAIL, self.indexer.remove('bar'))
 
     def test_ShouldRemoveInCorrectOrder(self):
-        self.server.index('foo', None)
-        self.server.index('bar', ['foo'])
-        self.server.index('baz', ['bar'])
+        self.indexer.index('foo', None)
+        self.indexer.index('bar', ['foo'])
+        self.indexer.index('baz', ['bar'])
 
-        self.assertEqual(OK, self.server.remove('baz'))
-        self.assertEqual(OK, self.server.remove('bar'))
-        self.assertEqual(OK, self.server.remove('foo'))
+        self.assertEqual(OK, self.indexer.remove('baz'))
+        self.assertEqual(OK, self.indexer.remove('bar'))
+        self.assertEqual(OK, self.indexer.remove('foo'))
 
 class Test_Server(unittest.TestCase):
     # * If the server doesn't recognise the command or if there's any problem with the message sent by the client it should return `ERROR\n`.
